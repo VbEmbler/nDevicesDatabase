b51673952a4ab7a099462a890a7bec62655b1555
Vladimir Bondarenko <vbembler@gmail.com>
2020-02-26 15:06:21 +0300

    Initial Commit

---
+++ DevicesDatabase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DevicesDatabase
+//
+//  Created by Vladimir on 26/02/2020.
+//  Copyright © 2020 Embler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
