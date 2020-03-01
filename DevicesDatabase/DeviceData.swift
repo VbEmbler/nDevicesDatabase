2633d7c586bd015c802dc293daecf0d511423873
Vladimir Bondarenko <vbembler@gmail.com>
2020-03-02 00:47:10 +0300

    added data storage to the DB

---
+++ DevicesDatabase/DeviceData.swift
@@ -0,0 +1,9 @@
+//
+//  DeviceData.swift
+//  DevicesDatabase
+//
+//  Created by Vladimir on 01/03/2020.
+//  Copyright © 2020 Embler. All rights reserved.
+//
+
+import Foundation
--- DevicesDatabase/ViewController.swift
+++ DevicesDatabase/ViewController.swift
@@ -10,10 +10,19 @@ import UIKit
 import Firebase
 
 class ViewController: UIViewController {
-
+    
+    
+    
     //Mark: Properties
+
+    let refDevices = Database.database().reference().child("devices")
+    var userAuthId: String!
+    
+    @IBOutlet weak var lblUserId: UILabel!
     @IBOutlet weak var lblConnectionStatus: UILabel!
+    @IBOutlet weak var tblViewDevices: UITableView!
     @IBAction func btnAddDevice(_ sender: UIButton) {
+        addDevise()
     }
     @IBAction func btnDeleteDevice(_ sender: UIButton) {
     }
@@ -21,7 +30,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-         Auth.auth().signInAnonymously() { (authResult, error) in
+
+        //Database.database().reference()
+        
+        Auth.auth().signInAnonymously() { (authResult, error) in
             
             if error != nil {
                 self.lblConnectionStatus.text = "Unable to connect"
@@ -29,13 +41,33 @@ class ViewController: UIViewController {
                 print("!!!!!!!!!!!!!!!!!!Error")
                 print(error!.localizedDescription)
             } else {
-                self.lblConnectionStatus.text = "Connected"
+                self.lblConnectionStatus.text = "Connected "
                 self.lblConnectionStatus.textColor = .green
+                
+                self.lblUserId.text = " \(authResult?.user.uid ?? "anon")"
                 print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Connected")
+                print(authResult!.user.uid)
+                self.userAuthId = authResult!.user.uid
             }
             
         }
+        
+        
+        //referenceDevices.setValue("Hi")
 
+    }
+    
+    
+    func addDevise (){
+        //let key = refDevices.childByAutoId().key
+        //let newDevice = refDevices.childByAutoId()
+        let device = [//"id": key,
+                      "userID": self.userAuthId,
+                      "deviceName": UIDevice.current.name,
+                      "deviceModel": UIDevice.current.model,
+                      "os_version": UIDevice.current.systemVersion
+                        ]
+        refDevices.child(self.userAuthId).setValue(device)
     }
 
 
