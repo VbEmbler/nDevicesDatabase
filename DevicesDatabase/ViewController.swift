f49bfd564fc7a82ea03eb3d186ea3569efaaabcd
Vladimir Bondarenko <vbembler@gmail.com>
2020-03-01 20:37:53 +0300

    added anonymous connection

--- DevicesDatabase/ViewController.swift
+++ DevicesDatabase/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    //Mark: Properties
+    @IBOutlet weak var lblConnectionStatus: UILabel!
+    @IBAction func btnAddDevice(_ sender: UIButton) {
+    }
+    @IBAction func btnDeleteDevice(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+         Auth.auth().signInAnonymously() { (authResult, error) in
+            
+            if error != nil {
+                self.lblConnectionStatus.text = "Unable to connect"
+                self.lblConnectionStatus.textColor = .red
+                print("!!!!!!!!!!!!!!!!!!Error")
+                print(error!.localizedDescription)
+            } else {
+                self.lblConnectionStatus.text = "Connected"
+                self.lblConnectionStatus.textColor = .green
+                print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Connected")
+            }
+            
+        }
 
     }
 
